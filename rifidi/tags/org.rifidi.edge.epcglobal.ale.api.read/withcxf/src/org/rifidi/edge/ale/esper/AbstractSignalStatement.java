2262026b29df857a5eb97e34de4a83af7396c872
kyle <kyle@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2009-06-30 17:54:50 +0000

    Before CXF removal

---
+++ rifidi/tags/org.rifidi.edge.epcglobal.ale.api.read/withcxf/src/org/rifidi/edge/ale/esper/AbstractSignalStatement.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package org.rifidi.edge.ale.esper;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+import org.rifidi.edge.core.services.notification.data.TagReadEvent;
+import org.rifidi.edge.epcglobal.aleread.ALEReadAPI;
+
+import com.espertech.esper.client.EPAdministrator;
+
+/**
+ * Extend this class to listen for results from esper statements.
+ * 
+ * @author Jochen Mader - jochen@pramari.com
+ * 
+ */
+public abstract class AbstractSignalStatement implements SignalStatement,
+		StatementController {
+	private CopyOnWriteArraySet<SignalListener> listeners;
+
+	protected EPAdministrator administrator;
+
+	public AbstractSignalStatement(EPAdministrator administrator) {
+		listeners = new CopyOnWriteArraySet<SignalListener>();
+		this.administrator = administrator;
+	}
+
+	/**
+	 * Helper method for generating a String to be used in esper inside a
+	 * every-distinct statement.
+	 * 
+	 * @param primarykeys
+	 * @return
+	 */
+	protected String assembleKeys(Set<String> primarykeys) {
+		StringBuilder builder = new StringBuilder();
+		for (String key : primarykeys) {
+			builder.append("res.tag.");
+			builder.append(key);
+			builder.append("?,");
+		}
+		builder.deleteCharAt(builder.length() - 1);
+		return builder.toString();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * esperplayground.esper.SignalStatement#registerSignalListener(esperplayground
+	 * .esper.SignalListener)
+	 */
+	@Override
+	public void registerSignalListener(SignalListener listener) {
+		listeners.add(listener);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @seeesperplayground.esper.SignalStatement#unregisterSignalListener(
+	 * esperplayground.esper.SignalListener)
+	 */
+	@Override
+	public void unregisterSignalListener(SignalListener listener) {
+		listeners.remove(listener);
+	}
+
+	/**
+	 * Shortcut to inform all listeners about a start signal.
+	 * 
+	 * @param type
+	 * @param cause
+	 */
+	protected void signalStart(ALEReadAPI.TriggerCondition condition,
+			Object cause) {
+		for (SignalListener listener : listeners) {
+			listener.startSignal(condition, cause);
+		}
+	}
+
+	/**
+	 * Shortcut to inform all listeners that a statement has stopped.
+	 * 
+	 * @param type
+	 * @param cause
+	 * @param events
+	 */
+	protected void signalStop(ALEReadAPI.TriggerCondition condition,
+			Object cause, List<TagReadEvent> events) {
+		for (SignalListener listener : listeners) {
+			listener.stopSignal(condition, cause, events);
+		}
+	}
+}
---
+++ rifidi/tags/org.rifidi.edge.epcglobal.ale.api.read/withcxf/src/org/rifidi/edge/epcglobal/aleread/ALEDataFormats.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.rifidi.edge.epcglobal.aleread;
+
+/**
+ * Formats of data stored on epc data sources.
+ * 
+ * @author Jochen Mader - jochen@pramari.com
+ * 
+ */
+public enum ALEDataFormats {
+	EPC_TAG, EPC_PURE, EPC_HEX, EPC_DECIMAL, HEX, DECIMAL, STRING, DEFAULT;
+}
