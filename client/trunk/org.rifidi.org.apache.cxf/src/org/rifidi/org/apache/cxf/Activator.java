782b53593080ad2a0cf2ced3c5c13398130bfdb4
matt <matt@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2011-12-05 21:09:54 +0000

    initial commit

---
+++ client/trunk/org.rifidi.org.apache.cxf/src/org/rifidi/org/apache/cxf/Activator.java
@@ -0,0 +1,62 @@
+/*
+ * Activator.java
+ * 
+ * Created:     July 8th, 2009
+ * Project:       Rifidi Edge Server - A middleware platform for RFID applications
+ *                    http://www.rifidi.org
+ *                    http://rifidi.sourceforge.net
+ * Copyright:   Pramari LLC and the Rifidi Project
+ * License:      The software in this package is published under the terms of the EPL License
+ *                    A copy of the license is included in this distribution under Rifidi-License.txt 
+ */
+package org.rifidi.org.apache.cxf;
+
+
+import org.eclipse.core.runtime.Plugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends Plugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.rifidi.edge.client.cxf";
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugins#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.core.runtime.Plugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
