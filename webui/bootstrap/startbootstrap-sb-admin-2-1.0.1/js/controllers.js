6ba7a2e53611b06db7471c96f0ebe6252900e0d6
manueltobon1 <manueltobon1@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2014-12-11 19:18:44 +0000

    ading google maps angular component

--- webui/bootstrap/startbootstrap-sb-admin-2-1.0.1/js/controllers.js
+++ webui/bootstrap/startbootstrap-sb-admin-2-1.0.1/js/controllers.js
@@ -75,6 +75,16 @@ $scope.servers = [
 
   //$scope.orderProp = 'age';
 });
+
+var myAppModule = angular.module('app.ui-map', ['ui.map']);
+
+myAppModule.controller('MapCtrl', ['$scope', function ($scope) {
+    $scope.mapOptions = {
+      center: new google.maps.LatLng(35.784, -78.670),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+  }]);
 	
 	
   
