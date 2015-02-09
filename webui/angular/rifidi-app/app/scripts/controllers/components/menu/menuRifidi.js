2696deeecfefe9331b2c5616984288c416b302ae
manueltobon1 <manueltobon1@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2015-02-09 16:19:31 +0000

    Add sensor wizard

--- webui/angular/rifidi-app/app/scripts/controllers/components/menu/menuRifidi.js
+++ webui/angular/rifidi-app/app/scripts/controllers/components/menu/menuRifidi.js
@@ -978,9 +978,6 @@ angular.module('rifidiApp')
           });
 
 
-
-
-
           $scope.tabs = [
               { title:'Dynamic Title 1', content:'Dynamic content 1' },
               { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
