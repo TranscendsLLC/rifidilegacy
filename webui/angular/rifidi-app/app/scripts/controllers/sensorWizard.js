bfc15cefa8b6f42b9127a2c997f23b6e81a0e0cf
manueltobon1 <manueltobon1@c725cd63-05c5-43f1-acf1-c8fb5b605cfe>
2015-02-11 22:49:30 +0000

    Sensor wizard: add field to capture custom id, add description on that field, query reader connection properties service, develop second step of wizard (create session) to display the connection properties for selected sensor in first step

--- webui/angular/rifidi-app/app/scripts/controllers/sensorWizard.js
+++ webui/angular/rifidi-app/app/scripts/controllers/sensorWizard.js
@@ -18,6 +18,9 @@ angular.module('rifidiApp')
       console.log("cleaning readerTypes: ");
       $scope.readerTypes = [];
       $scope.selectedReaderType = "";
+      $scope.readersConnectionProperties = [];
+      $scope.customReaderId = "";
+      $scope.selectedReaderConnectionProperties = [];
 
       //loadReaderTypes();
 
@@ -105,8 +108,153 @@ angular.module('rifidiApp')
               // or server returns response with an error status.
             });
 
-      //}
 
+      //load the connection properties
+      $http.get(host + '/readermetadata')
+          .success(function(data, status, headers, config) {
+
+
+            var xmlMetadata;
+            if (window.DOMParser)
+            {
+              var parser = new DOMParser();
+              xmlMetadata = parser.parseFromString(data,"text/xml");
+            }
+            else // Internet Explorer
+            {
+              xmlMetadata = new ActiveXObject("Microsoft.XMLDOM");
+              xmlMetadata.async=false;
+              xmlMetadata.loadXML(data);
+            }
+
+            //get the xml response and extract the values to construct the connection properties for all readers
+
+            var readerMetadataXmlVector = xmlMetadata.getElementsByTagName("reader");
+
+            //console.log("readerMetadataXmlVector.length: " + readerMetadataXmlVector.length);
+
+
+            for(var index = 0; index < readerMetadataXmlVector.length; index++) {
+
+
+              var readerid = readerMetadataXmlVector[index].getElementsByTagName("readerid")[0].childNodes[0];
+              var propertiesXmlVector = readerMetadataXmlVector[index].getElementsByTagName("property");
+
+              console.log("readerid: " + readerid.nodeValue);
+              console.log("properties length: " + propertiesXmlVector.length);
+              //console.log("propertiesXmlVector");
+              //console.log(propertiesXmlVector);
+
+              //Create a properties object for this reader
+              var readerConnectionProperties = {
+                "readerid": readerid.nodeValue,
+                "properties": []
+              };
+
+
+
+              for(var indexProp = 0; indexProp < propertiesXmlVector.length; indexProp++) {
+
+                console.log("indexProp: " + indexProp);
+
+
+                var name = propertiesXmlVector[indexProp].getElementsByTagName("name")[0].childNodes[0];
+                var displayname = propertiesXmlVector[indexProp].getElementsByTagName("displayname")[0].childNodes[0];
+                var description = propertiesXmlVector[indexProp].getElementsByTagName("description")[0].childNodes[0];
+                var type = propertiesXmlVector[indexProp].getElementsByTagName("type")[0].childNodes[0];
+                var category = propertiesXmlVector[indexProp].getElementsByTagName("category")[0].childNodes[0];
+                var writable = propertiesXmlVector[indexProp].getElementsByTagName("writable")[0].childNodes[0];
+                var ordervalue = propertiesXmlVector[indexProp].getElementsByTagName("ordervalue")[0].childNodes[0];
+
+                //if category equals to connection, then extract that property
+                if (category.nodeValue == 'connection'){
+
+                  var propertyElement = {
+                    "name": name.nodeValue,
+                    "displayname": displayname.nodeValue,
+                    "description": description.nodeValue,
+                    "type": type.nodeValue,
+                    "category": category.nodeValue,
+                    "writable": writable.nodeValue,
+                    "ordervalue": ordervalue.nodeValue
+                  };
+
+                  //Add the property to properties list
+                  readerConnectionProperties.properties.push(propertyElement);
+
+                }
+
+
+              }
+
+
+              $scope.readersConnectionProperties.push(readerConnectionProperties);
+
+
+            }
+
+            console.log("$scope.readersConnectionProperties");
+            console.log($scope.readersConnectionProperties);
+
+          })
+          .error(function(data, status, headers, config) {
+            console.log("error reading readermetadata for sensor wizard");
+
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+          });
+
+      $scope.prepareCreateSessionStep = function(){
+        console.log("prepareCreateSessionStep called");
+
+        //Clean variable
+        $scope.selectedReaderConnectionProperties = [];
+
+        console.log("$scope.selectedReaderType");
+        console.log($scope.selectedReaderType);
+
+        console.log("$scope.customReaderId");
+        console.log($scope.customReaderId);
+
+
+
+        //$scope.selectedReaderType = selectedReaderType;
+
+        //console.log("$scope.selectedReaderType");
+        //console.log($scope.selectedReaderType);
+
+        //console.log("selectedReaderType");
+        //console.log(selectedReaderType);
+
+        //load the properties for selected reader type
+        $scope.readersConnectionProperties.forEach(function(readerConnectionProperties){
+
+
+
+
+          if (readerConnectionProperties.readerid == $scope.selectedReaderType.factoryID){
+
+            console.log("match");
+
+            //Add the property
+            $scope.selectedReaderConnectionProperties.push(readerConnectionProperties.properties);
+
+          }
+
+        });
+
+        console.log("$scope.selectedReaderConnectionProperties");
+        console.log($scope.selectedReaderConnectionProperties);
+
+
+
+      }
+
+      $scope.readerTypeSelectAction = function(selectedReaderType){
+
+        $scope.selectedReaderType = selectedReaderType;
+        $scope.prepareCreateSessionStep();
+      }
 
 
   });
